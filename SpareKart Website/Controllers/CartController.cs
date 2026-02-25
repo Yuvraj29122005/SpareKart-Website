cf4b5161bb3eab39836f9ccfe7b4f22773cad359
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-25 09:03:21 +0530

    cart controller created

---
+++ SpareKart Website/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpareKart_Website.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpareKart_Website.Controllers
+{
+    public class CartController : Controller
+    {
+        public static List<CartItem> cart = new List<CartItem>();
+
+        public IActionResult Index()
+        {
+            return View(cart);
+        }
+
+        public IActionResult Add(string name, int price, string image)
+        {
+            var item = cart.FirstOrDefault(x => x.Name == name);
+
+            if (item != null)
+                item.Quantity++;
+            else
+                cart.Add(new CartItem
+                {
+                    Name = name,
+                    Price = price,
+                    Image = image,
+                    Quantity = 1
+                });
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Increase(string name)
+        {
+            var item = cart.FirstOrDefault(x => x.Name == name);
+            if (item != null) item.Quantity++;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Decrease(string name)
+        {
+            var item = cart.FirstOrDefault(x => x.Name == name);
+            if (item != null && item.Quantity > 1)
+                item.Quantity--;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Remove(string name)
+        {
+            var item = cart.FirstOrDefault(x => x.Name == name);
+            if (item != null)
+                cart.Remove(item);
+            return RedirectToAction("Index");
+        }
+    }
+}
