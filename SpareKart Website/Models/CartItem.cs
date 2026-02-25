bce9001d9a17116a980612bcf0a3731dedca1be0
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-25 09:01:17 +0530

    cart model created

---
+++ SpareKart Website/Models/CartItem.cs
@@ -0,0 +1,10 @@
+﻿namespace SpareKart_Website.Models
+{
+    public class CartItem
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+    }
+}
