bc3cc59633f16e04c362c6cba6a432181b36299e
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-25 10:34:53 +0530

    create the feedback page with css

---
+++ SpareKart Website/Controllers/FeedbackController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpareKart_Website.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Submit()
+        {
+            TempData["msg"] = "Thank you for your valuable feedback!";
+            return RedirectToAction("Index");
+        }
+    }
+}
