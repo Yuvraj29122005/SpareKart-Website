23bc99da893e527449dbb27b35b7d34c6bdd8775
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-25 10:04:34 +0530

    active navbar and change design

---
+++ SpareKart Website/Controllers/ProfileController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpareKart_Website.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
