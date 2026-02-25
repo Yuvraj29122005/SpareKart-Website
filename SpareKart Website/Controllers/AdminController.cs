89c8ec69047afaee5c88cd1df7d31bab9b5db981
Shreya-Dhaduk <shreyadhaduk2105@gmail.com>
2026-02-25 11:27:21 +0530

    admn panel changes

--- SpareKart Website/Controllers/AdminController.cs
+++ SpareKart Website/Controllers/AdminController.cs
@@ -4,10 +4,26 @@ namespace SpareKartAdmin.Controllers
 {
     public class AdminController : Controller
     {
+
         public IActionResult Dashboard()
         {
             return View();
         }
+
+        public IActionResult Products()
+        {
+            return View();
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        public IActionResult EditProduct()
+        {
+            return View();
+        }
+
     }
 }
-//shreya
--- SpareKart Website/Controllers/ProductController.cs
+++ SpareKart Website/Controllers/ProductController.cs
@@ -1,12 +1,66 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace SpareKart_Website.Controllers
+namespace SpareKartAdmin.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult ProductDetails()
+        static List<Product> products = new List<Product>()
+        {
+            new Product{Id=1,Name="Engine Oil Filter",Category="Engine",Price=499,Stock=50},
+            new Product{Id=2,Name="Brake Disc Set",Category="Brakes",Price=2999,Stock=30}
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+
+        public IActionResult AddProduct()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product p)
+        {
+            p.Id = products.Count + 1;
+            products.Add(p);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditProduct(int id)
+        {
+            var data = products.FirstOrDefault(x => x.Id == id);
+            return View(data);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(Product p)
+        {
+            var data = products.FirstOrDefault(x => x.Id == p.Id);
+            data.Name = p.Name;
+            data.Category = p.Category;
+            data.Price = p.Price;
+            data.Stock = p.Stock;
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var data = products.FirstOrDefault(x => x.Id == id);
+            products.Remove(data);
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
     }
 }
