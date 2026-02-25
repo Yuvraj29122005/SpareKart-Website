00cdbfd64bbfd6f31ba7907c91c267548868541d
Shreya-Dhaduk <shreyadhaduk2105@gmail.com>
2026-02-25 13:28:58 +0530

    edit & delete product page created

--- SpareKart Website/Controllers/ProductController.cs
+++ SpareKart Website/Controllers/ProductController.cs
@@ -1,20 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SpareKartAdmin.Controllers
 {
     public class ProductController : Controller
     {
-        static List<Product> products = new List<Product>()
-        {
-            new Product{Id=1,Name="Engine Oil Filter",Category="Engine",Price=499,Stock=50},
-            new Product{Id=2,Name="Brake Disc Set",Category="Brakes",Price=2999,Stock=30}
-        };
-
         public IActionResult Index()
         {
-            return View(products);
+            return View();
         }
 
         public IActionResult AddProduct()
@@ -22,45 +14,9 @@ namespace SpareKartAdmin.Controllers
             return View();
         }
 
-        [HttpPost]
-        public IActionResult AddProduct(Product p)
+        public IActionResult EditProduct()
         {
-            p.Id = products.Count + 1;
-            products.Add(p);
-            return RedirectToAction("Index");
+            return View();
         }
-
-        public IActionResult EditProduct(int id)
-        {
-            var data = products.FirstOrDefault(x => x.Id == id);
-            return View(data);
-        }
-
-        [HttpPost]
-        public IActionResult EditProduct(Product p)
-        {
-            var data = products.FirstOrDefault(x => x.Id == p.Id);
-            data.Name = p.Name;
-            data.Category = p.Category;
-            data.Price = p.Price;
-            data.Stock = p.Stock;
-            return RedirectToAction("Index");
-        }
-
-        public IActionResult Delete(int id)
-        {
-            var data = products.FirstOrDefault(x => x.Id == id);
-            products.Remove(data);
-            return RedirectToAction("Index");
-        }
-    }
-
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Category { get; set; }
-        public int Price { get; set; }
-        public int Stock { get; set; }
     }
 }
