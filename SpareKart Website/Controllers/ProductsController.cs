6c7ffcca2758ce2968e597595cde3e472fa38231
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-27 18:52:09 +0530

    make a products page isue solved and re create

--- SpareKart Website/Controllers/ProductsController.cs
+++ SpareKart Website/Controllers/ProductsController.cs
@@ -4,9 +4,23 @@ namespace SpareKart_Website.Controllers
 {
     public class ProductsController : Controller
     {
+
+        // ALL PRODUCTS PAGE
         public IActionResult AllProducts()
         {
             return View();
         }
+
+
+        // 🔥 PRODUCT DETAILS PAGE (THIS IS MISSING IN YOUR PROJECT)
+        [HttpGet]
+        public IActionResult ProductDetails(string name, int price, string image)
+        {
+            ViewBag.Name = name;
+            ViewBag.Price = price;
+            ViewBag.Image = image;
+            return View();
+        }
+
     }
 }
