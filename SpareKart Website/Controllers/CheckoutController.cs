e9d7efac7a34facfbf1ffc2e6026b49a977d3592
Yuvraj29122005 <yuvrajdhadhal988@gmail.com>
2026-02-28 18:19:02 +0530

    changes in file structure

--- SpareKart Website/Controllers/CheckoutController.cs
+++ SpareKart Website/Controllers/CheckoutController.cs
@@ -7,16 +7,22 @@ namespace SpareKart_Website.Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index(string name, int price, string image)
+        // GET - show checkout page with full cart
+        public IActionResult Index()
         {
-            ViewBag.Name = name;
-            ViewBag.Price = price;
-            ViewBag.Image = image;
+            // Pass the entire cart to the view
+            var cart = CartController.cart;
+
+            if (cart == null || cart.Count == 0)
+                return RedirectToAction("Index", "Cart");
+
+            ViewBag.Cart = cart;
+            ViewBag.Total = cart.Sum(x => x.Price * x.Quantity);
             return View();
         }
 
         [HttpPost]
-        public IActionResult PlaceOrder(string address)
+        public IActionResult PlaceOrder(string address, string fullname, string email, string phone, string pay)
         {
             if (CartController.cart.Count == 0)
                 return RedirectToAction("Index", "Home");
